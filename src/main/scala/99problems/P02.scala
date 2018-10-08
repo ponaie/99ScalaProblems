f797f9fc18eff0e5f2eb65fa5cfcfca9d9b46c11
ponaie <xxueqiang16@gmail.com>
2018-10-08 10:04:09 +0800

    Update P02

--- src/main/scala/99problems/P02.scala
+++ src/main/scala/99problems/P02.scala
@@ -6,7 +6,7 @@
 object P02 {
   // Built-in
   def penultimateBuiltin[A](list: List[A]): A = {
-    if(list.isEmpty) throw new NoSuchElementException
+    if (list.isEmpty) throw new NoSuchElementException
     else list.init.last
   }
 
@@ -15,5 +15,28 @@ object P02 {
     case e :: _ :: Nil => e
     case _ :: tail => penultimateRecursive(tail)
     case _ => NoSuchElementException
+  }
+
+  // lastNth Builtin
+  def lastNthBuiltin[A](n: Int, ls: List[A]): A = {
+    if (n <= 0) throw new IllegalArgumentException
+    if (ls.length < n) throw new NoSuchElementException
+    ls.takeRight(n).head
+  }
+
+  // Recursive(similar to two pointers approach)
+  def lastNthRecursive[A](n: Int, ls: List[A]): A = {
+    def lastNthR(count: Int, resultList: List[A], curList: List[A]): A =
+      curList match {
+        case Nil if count > 0 => throw new NoSuchElementException
+        case Nil => resultList.head
+        case _ :: tail =>
+          lastNthR(
+            count - 1,
+            if (count > 0) resultList else resultList.tail,
+            tail)
+      }
+    if (n <= 0) throw new IllegalArgumentException
+    else lastNthR(n, ls, ls)
   }
 }
